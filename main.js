f12a1cf9352a99c113c5383dc7fe1a3f2e9a8994
= <=>
2021-07-29 21:56:52 +0100

    intial commit

--- main.js
+++ main.js
@@ -8,4 +8,10 @@ function draw(){
     ctx.fillRect(0, 0, canvas.width, canvas.height)
 }
 
-setInterval(draw, 5)
+x = new Vector2(3, 4)
+y = new Vector2(1, 1)
+console.log(x.sizeof())
+console.log(x.add(y))
+
+
+//setInterval(draw, 5)
---
+++ physics_fundamentals.js
@@ -0,0 +1,19 @@
+class Vector2 {
+    constructor (x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    add(v){
+        return (new Vector2(this.x + v.x, this.y + v.y))
+    }
+
+    subtract(v){
+        return (new Vector2(this.x - v.x, this.y - v.y))
+    }
+
+    sizeof(){
+        return (Math.sqrt(Math.pow(this.x, 2), Math.pow(this.y, 2)))
+    }
+
+}
