0502874c4c198d8f376db672c78f9a1a4623f401
= <=>
2021-07-29 21:43:18 +0100

    inital commit

---
+++ main.js
@@ -0,0 +1,11 @@
+canvas = document.getElementById("myCanvas")
+ctx = canvas.getContext("2d")
+
+canvas.width = 600
+canvas.height = 600
+
+function draw(){
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+}
+
+setInterval(draw, 5)
