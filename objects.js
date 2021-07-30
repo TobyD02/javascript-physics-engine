6e1a4b792a048121b1700fab2174fa247001eced
Toby Dixon <thetjdixon1234@gmail.com>
2021-07-30 21:59:11 +0100

    added Square class

--- objects.js
+++ objects.js
@@ -16,7 +16,7 @@ class AABB{
 
 // Circle
 
-class Circle {
+class Circle{
     constructor(radius, position){
         this.radius = radius
         this.pos = position
@@ -41,4 +41,78 @@ class Circle {
     setVelocity(v){
         this.velocity = velocity
     }
-}   
+
+    draw(ctx){
+
+    }
+
+    update(){}
+}
+
+// Square
+
+class Square{
+    constructor(origin, size){
+        this.size = size
+        this.origin = origin
+        this.coords = [
+            origin,
+            new Vector2(origin.x + this.size, origin.y),
+            new Vector2(origin.x + this.size, origin.y + this.size),
+            new Vector2(origin.x, origin.y + this.size)
+        ]
+
+        this.maxSpeed = 5
+        this.acceleration = new Vector2(10, 5)
+        this.velocity = new Vector2(0, 0)
+
+        this.friction = 0.01
+
+    }
+
+    draw(ctx){
+        ctx.beginPath()
+        ctx.moveTo(this.coords[0].x, this.coords[0].y)
+        ctx.lineTo(this.coords[1].x, this.coords[1].y)
+        ctx.lineTo(this.coords[2].x, this.coords[2].y)
+        ctx.lineTo(this.coords[3].x, this.coords[3].y)
+        ctx.lineTo(this.coords[0].x, this.coords[0].y)
+        ctx.stroke()
+        ctx.closePath()
+    }
+
+    update(){
+
+        // Movement
+        let distanceToEnd = this.velocity.magnitude()
+        if (distanceToEnd < this.maxSpeed) this.velocity = this.velocity.add(this.acceleration)
+
+        this.coords.forEach((pos) => {
+            if (pos.x + this.velocity.x > canvas.width || pos.x + this.velocity.x < 0) {
+                this.velocity.x = -this.velocity.x
+                this.acceleration.x = -this.acceleration.x
+            }
+
+            if (pos.y + this.velocity.y > canvas.height || pos.y + this.velocity.y < 0){
+                this.velocity.y = -this.velocity.y
+                this.acceleration.y = -this.acceleration.y
+            }
+        })
+
+        this.origin = this.origin.add(this.velocity)
+
+        this.coords = [
+            this.origin,
+            new Vector2(this.origin.x + this.size, this.origin.y),
+            new Vector2(this.origin.x + this.size, this.origin.y + this.size),
+            new Vector2(this.origin.x, this.origin.y + this.size)
+        ]
+
+        this.acceleration = this.acceleration.subtract(this.acceleration.multiplyByScalar(this.friction))
+
+        this.velocity = this.velocity.multiplyByScalar(this.friction)
+        if (this.velocity.magnitude() < 0.1) this.velocity = new Vector2(0, 0)
+        if (this.acceleration.magnitude() < 0.1) this.acceleration = new Vector2(0, 0)
+    }
+
+}
