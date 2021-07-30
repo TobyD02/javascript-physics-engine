7323f115058d6816d7466f132854f7563101fe5d
Toby Dixon <thetjdixon1234@gmail.com>
2021-07-30 22:00:16 +0100

    added testing code for new Square class

--- main.js
+++ main.js
@@ -4,8 +4,15 @@ ctx = canvas.getContext("2d")
 canvas.width = 600
 canvas.height = 600
 
+let a = new Square(new Vector2(10, 10), 50)
+
 function draw(){
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.strokeRect(0, 0, canvas.width, canvas.height)
+    a.update()
+    a.draw(ctx)
+    // console.log(a.origin)
 }
 
-//setInterval(draw, 5)  
+
+setInterval(draw, 5)  
