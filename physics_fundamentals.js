f07d5f839013b16c67f6b27f6dbe14863631c13e
Toby Dixon <thetjdixon1234@gmail.com>
2021-07-30 21:58:52 +0100

    added new Vector2 oeprators

--- physics_fundamentals.js
+++ physics_fundamentals.js
@@ -5,21 +5,23 @@ class Vector2 {
     }
 
     add(v){
-        this.x + v.x
-        this.y + v.y
+        return new Vector2(this.x + v.x, this.y + v.y)
     }
 
     subtract(v){
-        this.x - v.x
-        this.y - v.y
+        return new Vector2(this.x - v.x, this.y - v.y)
     }
 
     distance(v){
         return (Math.sqrt(Math.pow((this.x - v.x), 2) + Math.pow((this.y - v.y), 2)))
     }
 
+    magnitude(){
+        return Math.abs(Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2)))
+    }
+
     midpoint(v){
-        return (new Vector2((this.x + v.x) / 2), (this.y + v.y) / 2)
+        return new Vector2((this.x + v.x) / 2, (this.y + v.y) / 2)
     }
 
     dotProduct(v){
