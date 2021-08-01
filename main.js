ffdb897731354ae4095717654a40965c7ca2cc89
Toby Dixon <thetjdixon1234@gmail.com>
2021-08-01 19:55:19 +0100

    Testing code for rotation

--- main.js
+++ main.js
@@ -9,10 +9,15 @@ let a = new Square(new Vector2(10, 10), 50)
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.strokeRect(0, 0, canvas.width, canvas.height)
-    a.update()
+    // a.update()
     a.draw(ctx)
+    a.rotate(0.005)
+
     // console.log(a.origin)
 }
 
 
-setInterval(draw, 5)  
+setInterval(draw, 4)
+
+
+// setInterval(draw, 5)  
